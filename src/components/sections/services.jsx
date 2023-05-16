37615a55c02a073bda076999f8ca2965b8f4b1ee
Jay Sanduke <Jaysanduke@gmail.com>
2023-05-16 22:26:32 +0530

    Update services.jsx

--- src/components/sections/services.jsx
+++ src/components/sections/services.jsx
@@ -13,7 +13,7 @@ export default function services() {
                     <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
                         <div className="card">
                             <div className="card-img">
-                                <img src="/src/assets/img/img5.jpg" alt="" className="img-fluid" />
+                                <img src="src/assets/img/img5.jpg" alt="" className="img-fluid" />
                             </div>
                             <h3><a href="#" className="stretched-link">Air Quality Index</a></h3>
                             <p>The air quality index (AQI) is a measure used to report daily air quality levels and the potential health risks associated with air pollution. It is typically calculated for specific geographic locations and includes measurements of various pollutants, such as particulate matter, ozone, nitrogen dioxide, and sulfur dioxide.</p>
@@ -23,7 +23,7 @@ export default function services() {
                     <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
                         <div className="card">
                             <div className="card-img">
-                                <img src="/src/assets/img/img4.jpg" alt="" className="img-fluid" />
+                                <img src="src/assets/img/img4.jpg" alt="" className="img-fluid" />
                             </div>
                             <h3><a href="#" className="stretched-link">Plant Recommendation</a></h3>
                             <p>We recommend best plants to individuals about what types of plants would be suitable for their specific needs, preferences, and growing conditions. This could include recommendations for indoor plants, outdoor plants, ornamental plants, edible plants, or plants that have specific health or environmental benefits.</p>
@@ -33,7 +33,7 @@ export default function services() {
                     <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
                         <div className="card">
                             <div className="card-img">
-                                <img src="/src/assets/img/img6.jpg" alt="" className="img-fluid" />
+                                <img src="src/assets/img/img6.jpg" alt="" className="img-fluid" />
                             </div>
                             <h3><a href="#" className="stretched-link">Plant Store</a></h3>
                             <p>We are an establishment that specializes in the sale of plants and related gardening supplies. We offer a wide variety of plants, including indoor and outdoor plants, ornamental plants, succulents, trees, shrubs, and seasonal plants.</p>
@@ -43,7 +43,7 @@ export default function services() {
                     <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
                         <div className="card">
                             <div className="card-img">
-                                <img src="/src/assets/img/img7.jpg" alt="" className="img-fluid" />
+                                <img src="src/assets/img/img7.jpg" alt="" className="img-fluid" />
                             </div>
                             <h3><a href="#" className="stretched-link">Gardening Tips</a></h3>
                             <p>Our gardening tips cover a wide range of topics, including soil preparation, plant selection, planting techniques, watering and fertilizing, pest control, pruning, and harvesting.
@@ -54,7 +54,7 @@ export default function services() {
                     <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
                         <div className="card">
                             <div className="card-img">
-                                <img src="/src/assets/img/img8.jpg" alt="" className="img-fluid" />
+                                <img src="src/assets/img/img8.jpg" alt="" className="img-fluid" />
                             </div>
                             <h3><a href="#" className="stretched-link">Community</a></h3>
                             <p>Our community share a common interest, identity, or geographic location and people can interact with one another and work together toward common goals.
@@ -65,7 +65,7 @@ export default function services() {
                     <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
                         <div className="card">
                             <div className="card-img">
-                                <img src="/src/assets/img/img9.jpg" alt="" className="img-fluid" />
+                                <img src="src/assets/img/img9.jpg" alt="" className="img-fluid" />
                             </div>
                             <h3><a href="#" className="stretched-link">Enviorment Analysis</a></h3>
                             <p>Environmental analysis may be used to identify potential environmental risks and opportunities, inform decision-making, and develop strategies for sustainable environmental management. It can also be used to assess compliance with environmental regulations and standards, as well as to monitor changes in environmental conditions over time.</p>
